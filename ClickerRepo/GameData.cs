e8babe78b9d30b8129348de4027c074ad88dacbd
Johnson Lee <johnson.hwang1008@gmail.com>
2021-09-21 18:30:34 -0400

    first commit

---
+++ ClickerRepo/GameData.cs
@@ -0,0 +1,20 @@
+﻿namespace ClickerRepo
+{
+    public class GameData
+    {
+        public int Cash { get; set; }
+        public int Value { get; set; }
+        public int Min { get; set; }
+        public int Min2 { get; set; }
+
+        public GameData(int yourCash, int dollarValue, int min, int min2)
+        {
+            Cash = yourCash;
+            Value = dollarValue;
+            Min = min;
+            Min2 = min2;
+        }
+
+
+    }
+}
